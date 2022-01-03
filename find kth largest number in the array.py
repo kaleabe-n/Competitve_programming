37bdc8d06aed6e5728c8d12ebf0c394ecfe5b8b9
kaleabe-n <91533360+kaleabe-n@users.noreply.github.com>
2022-01-03 20:48:32 +0300

    Create find kth largest number in the array.py

---
+++ find kth largest number in the array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        for i in range(len(nums)):
+            nums[i]=int(nums[i])
+        nums.sort()
+        return str(nums[-k])
+        """
+        :type nums: List[str]
+        :type k: int
+        :rtype: str
+        """
+        
