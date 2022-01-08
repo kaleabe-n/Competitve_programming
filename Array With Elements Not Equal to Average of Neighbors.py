844cea459312d4bc2bda9535feb40c590aa7cc7b
kaleabe-n <91533360+kaleabe-n@users.noreply.github.com>
2022-01-09 01:29:56 +0300

    Create Array With Elements Not Equal to Average of Neighbors.py

---
+++ Array With Elements Not Equal to Average of Neighbors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        nums.sort()
+        for i in range(0, len(nums) - 1, 2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
