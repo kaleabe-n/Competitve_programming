49e30690a1726986647ad2bf457c42dcd9f8c6ab
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-04-20 02:40:57 +0300

    Create find original array from doubled array.py

---
+++ DQ/find original array from doubled array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findOriginalArray(self, changed):
+        changed.sort()
+        originalLength = len(changed)//2
+        i = 0
+        j = 1
+        while len(changed) > originalLength:
+            if j>=len(changed):
+                return []
+            elif changed[i]*2 == changed[j] and i != j:
+                changed.pop(j)
+                i+=1
+            else:
+                j+=1
+        if(len(changed) == originalLength):
+            return changed
+        else:
+            return []
+        """
+        :type changed: List[int]
+        :rtype: List[int]
+        """
