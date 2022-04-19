b7d42a02bff87d34910b5c992e9a3efc05138587
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-04-20 02:12:09 +0300

    Create minimum increment to make array unique.py

---
+++ minimum increment to make array unique.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        nums.sort()
+        i = 0
+        inc = 0
+        while i<(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i+1]+=1
+                inc+=1
+                print("this")
+            elif (nums[i] > nums[i+1]):
+                temp = (nums[i]-nums[i+1]+1)
+                nums[i+1]+= temp
+                inc+=temp
+            i+=1
+        return inc
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
