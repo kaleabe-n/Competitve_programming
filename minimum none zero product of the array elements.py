881575f43490de324be719037c3f2369372acc92
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-05-31 01:11:35 +0300

    Create minimum none zero product of the array elements.py

---
+++ minimum none zero product of the array elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minNonZeroProduct(self, p):
+        def power(a, b):
+            if b == 0:
+                return 1
+            ret = power(a, b>>1)
+            ret = (ret*ret)%1000000007
+            if b&1:
+                ret = (ret*a)%1000000007
+            return ret
+        return (power(2**p-2, 2**(p-1)-1)*(2**p-1))%1000000007
+
+        """
+        :type p: int
+        :rtype: int
+        """
+        
