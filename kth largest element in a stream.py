f8f458cc27a0e21b963ca330b7e4c8e05345a596
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-07-11 00:59:46 +0300

    Create kth largest element in a stream.py

---
+++ kth largest element in a stream.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.heap = nums
+        heapq.heapify(self.heap)
+        self.num = k
+
+    def add(self, val):
+        heapq.heappush(self.heap, val)
+        for i in range(len(self.heap)-self.num):
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
