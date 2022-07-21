99d989e61da357fcd39109f569d3c8d9e14a779a
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-07-21 19:29:09 +0300

    Update find the winner of circular game.py

--- find the winner of circular game.py
+++ find the winner of circular game.py
@@ -1,13 +1,16 @@
 class Solution(object):
     def findTheWinner(self, n, k):
-        l = list(range(1,n+1))
-        current = 0
-        while len(l)>1:
-            current = (current-1+k)%len(l)
-            l.pop(current)
-        return l[0]
+        def find(arr,k,curr):
+                if len(arr) == 1:
+                    return arr[0]
+                curr = (curr + k - 1)%len(arr)
+                arr.pop(curr)
+                return find(arr,k,curr)
+        
+        return find(list(range(1,n+1)),k,0)
         """
         :type n: int
         :type k: int
         :rtype: int
         """
+        
