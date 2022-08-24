da3a60fec49710a5c54f33080e1bf53e0bb864cc
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-08-24 22:28:12 +0300

    Update heap sort.py

--- heap sort.py
+++ heap sort.py
@@ -1,29 +1,30 @@
 
+#User function Template for python3
+
 class Solution:
+    
     #Heapify function to maintain heap property.
     def heapify(self,arr, n, i):
         # code here
-        if i >= 1 and i<n and 2*i<n and arr[i]>arr[2*i]:
-            arr[i],arr[2*i] = arr[2*i],arr[i]
-            self.heapify(arr,n,2*i)
-        elif i >= 1 and i<n and 2*i<n and arr[i]>arr[2*i+1] and i<n:
-            arr[i],arr[2*i+1] = arr[2*i+1],arr[i]
-            self.heapify(arr,n,2*i)
-        elif i<n and i>1 and arr[i] < arr[i//2]:
-            arr[i],arr[i//2] = arr[i//2],arr[i]
-            self.heapify(arr,n,i//2)
-        else:
-            return
+        large = i
+        if i*2+1<n and arr[i]<arr[i*2+1]:
+            large = i*2+1
+        if i*2+2<n and arr[i]<arr[i*2+2]:
+            if large is None or arr[large]<arr[2*i+2]:
+                large = i*2+2
+        if i!=large:
+            arr[large],arr[i]=arr[i],arr[large]
+            self.heapify(arr,n,large)
     #Function to build a Heap from array.
     def buildHeap(self,arr,n):
         # code here
-        arr.sort()
-        arr.insert(0,None)
-        for i in range(n):
-            self.heapify(arr,n,i)
+        for i in range(len(arr) - 1, -1, -1):
+            self.heapify(arr, len(arr), i)
     
     #Function to sort an array using Heap Sort.    
     def HeapSort(self, arr, n):
         #code here
         self.buildHeap(arr,n)
-        arr.pop(0)
+        for i in range(n-1,-1,-1):
+            arr[0],arr[i] = arr[i],arr[0]
+            self.heapify(arr,i,0)
