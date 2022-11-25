baeaf8594340e5fb902a5c3c58991ef102322322
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-11-25 15:55:03 +0300

    Time: 3840 ms (5.12%), Space: 39 MB (7.69%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,22 @@
+import heapq
+
+class Solution(object):
+    def maxResult(self, nums, k):
+        heap = [[-nums[-1],len(nums)-1]]
+        dp = [None]*len(nums)
+        dp[-1] = nums[-1]
+        for i in range(len(nums)-2,-1,-1):
+            if heap[0][1]>i+k:
+                while heap[0][1]>i+k:
+                    heapq.heappop(heap)
+            dp[i] = -heap[0][0] + nums[i]
+            heapq.heappush(heap,[-dp[i],i])
+        return dp[0]
+        
+                
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
