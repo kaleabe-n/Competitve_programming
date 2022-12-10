55d69c2004b08618ea678d268d73902d8f01face
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-12-10 13:03:21 +0300

    Time: 427 ms (34.90%), Space: 15.2 MB (81.35%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        i = 0
+        lenP = len(p)
+        p = Counter(p)
+        window = {}
+        ans = []
+        while i<len(s):
+            if s[i] in window:
+                window[s[i]]+=1
+            else:
+                window[s[i]] = 1
+            if i>=lenP:
+                if window[s[i-lenP]]>1:
+                    window[s[i-lenP]]-=1
+                else:
+                    window.pop(s[i-lenP])
+            if window == p and i>=lenP-1:
+                ans.append(i-lenP+1)
+            i+=1
+        return ans
