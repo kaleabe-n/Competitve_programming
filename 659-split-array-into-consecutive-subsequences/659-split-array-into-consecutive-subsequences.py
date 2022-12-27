015548e03020b74699f01198b19a2bcb0cf55467
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2022-12-27 11:10:33 +0300

    Time: 763 ms (71.95%), Space: 15.5 MB (11.48%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -1,26 +1,29 @@
+import heapq
+
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         expect = {}
         for i in nums:
             if i in expect:
                 if len(expect[i]) == 0:
-                    expect[i+1].append([i])
+                    heapq.heappush(expect[i+1],[1,[i]])
                 else:
-                    temp = expect[i].pop()
-                    temp.append(i)
-                    if i+1 in expect:
-                        expect[i+1].append(temp)
-                        expect[i+1].sort(key = lambda x:-len(x))
+                    temp = heapq.heappop(expect[i])
+                    temp[1].append(i)
+                    temp[0]+=1
+                    if (i+1) in expect:
+                        heapq.heappush(expect[i+1],temp)
                     else:
                         expect[i+1] = [temp]
             else:
                 if i+1 in expect:
-                    expect[i+1].append([i])
+                    heapq.heappush(expect[i+1],[1,[i]])
                 else:
-                    expect[i+1] = [[i]]
+                    
+                    expect[i+1] = [[1,[i]]]
         for i in expect:
             for j in expect[i]:
-                if 0<len(j)<=2:
+                if 0<len(j[1])<=2:
                     return False
         return True
         
