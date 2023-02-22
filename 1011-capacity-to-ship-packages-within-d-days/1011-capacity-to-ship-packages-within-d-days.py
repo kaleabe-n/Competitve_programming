5d845a2bf51d5105dc4127e7376bcf4b20b3870c
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-02-22 21:12:52 +0300

    Time: 1191 ms (7.89%), Space: 17.1 MB (78.06%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left = 1
+        right = sum(weights)
+        while left<right:
+            if not isAble(weights,days,(left+right)//2):
+                if isAble(weights,days,(left+right)//2+1):
+                    return (left+right)//2+1
+                else:
+                    left = (left+right)//2+1
+            else:
+                right = (left+right)//2
+        return left
+    
+    
+def isAble(weights,days,capacity):
+    i = 0
+    while days>0:
+        temp = capacity
+        while temp>0 and i<len(weights):
+            temp-=weights[i]
+            i+=1
+        if temp<0:
+            i-=1
+        days-=1
+    return i==len(weights)
