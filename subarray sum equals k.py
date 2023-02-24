b4f8be0985932088845992812ebd87cc531b0e8c
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-02-24 16:16:09 +0300

    Update subarray sum equals k.py

--- subarray sum equals k.py
+++ subarray sum equals k.py
@@ -1,23 +1,35 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        prefixSumCount = {0:1}
-        prefixSum = 0
-        count = 0
-        for i in nums:
-            prefixSum+=i
-            try:
-                count+=prefixSumCount[prefixSum-k]
-            except:
-                pass
-            if prefixSum in prefixSumCount:
-                prefixSumCount[prefixSum]+=1
-            else:
-                prefixSumCount[prefixSum] = 1
-        return count
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSumCount = collections.defaultdict(int)
+        prefixSumCount[0]=1
+        prefix = 0
+        subarrayCount = 0
+        for num in nums:
+            prefix+=num
+            subarrayCount+=prefixSumCount[prefix-k]
+            prefixSumCount[prefix]+=1
+        return subarrayCount
+
+# class Solution(object):
+#     def subarraySum(self, nums, k):
+#         prefixSumCount = {0:1}
+#         prefixSum = 0
+#         count = 0
+#         for i in nums:
+#             prefixSum+=i
+#             try:
+#                 count+=prefixSumCount[prefixSum-k]
+#             except:
+#                 pass
+#             if prefixSum in prefixSumCount:
+#                 prefixSumCount[prefixSum]+=1
+#             else:
+#                 prefixSumCount[prefixSum] = 1
+#         return count
             
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
         
