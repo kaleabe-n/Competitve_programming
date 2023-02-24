3944b7b44fca08e0b82c8cc4ea9b8c62e6dff457
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-02-24 17:46:26 +0300

    Update 907-sum-of-subarray-minimums.py

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,36 +1,58 @@
-class Solution(object):
-    def sumSubarrayMins(self, arr):
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
         indSta = [None]
         monSta = [float('inf')]
         prefixMonSta = [None]
-        i = 0
         ans = 0
-        while i<len(arr):
-            if arr[i]<=monSta[-1]:
-                while monSta and arr[i]<=monSta[-1]:
-                    monSta.pop()
-                    indSta.pop()
-                    prefixMonSta.pop()
-                monSta.append(arr[i])
-                if len(indSta)>0:
-                    prefix = prefixMonSta[-1]+(i-indSta[-1])*monSta[-1]
-                else:
-                    prefix = (i+1)*monSta[-1]
-                prefixMonSta.append(prefix)
-                indSta.append(i)
+        for i in range(len(arr)):
+            while monSta and arr[i]<=monSta[-1]:
+                monSta.pop()
+                indSta.pop()
+                prefixMonSta.pop()
+            monSta.append(arr[i])
+            if len(indSta)>0:
+                prefix = prefixMonSta[-1]+(i-indSta[-1])*monSta[-1]
             else:
-                monSta.append(arr[i])
-                if len(indSta)>0:
-                    prefix = prefixMonSta[-1]+(i-indSta[-1])*monSta[-1]
-                else:
-                    prefix = (i+1)*monSTa[-1]
-                prefixMonSta.append(prefix)
-                indSta.append(i)
+                prefix = (i+1)*monSta[-1]
+            prefixMonSta.append(prefix)
+            indSta.append(i)
             ans+=prefixMonSta[-1]
-            i+=1
         return ans%(10**9+7)
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
+
+
+# class Solution(object):
+#     def sumSubarrayMins(self, arr):
+#         indSta = [None]
+#         monSta = [float('inf')]
+#         prefixMonSta = [None]
+#         i = 0
+#         ans = 0
+#         while i<len(arr):
+#             if arr[i]<=monSta[-1]:
+#                 while monSta and arr[i]<=monSta[-1]:
+#                     monSta.pop()
+#                     indSta.pop()
+#                     prefixMonSta.pop()
+#                 monSta.append(arr[i])
+#                 if len(indSta)>0:
+#                     prefix = prefixMonSta[-1]+(i-indSta[-1])*monSta[-1]
+#                 else:
+#                     prefix = (i+1)*monSta[-1]
+#                 prefixMonSta.append(prefix)
+#                 indSta.append(i)
+#             else:
+#                 monSta.append(arr[i])
+#                 if len(indSta)>0:
+#                     prefix = prefixMonSta[-1]+(i-indSta[-1])*monSta[-1]
+#                 else:
+#                     prefix = (i+1)*monSTa[-1]
+#                 prefixMonSta.append(prefix)
+#                 indSta.append(i)
+#             ans+=prefixMonSta[-1]
+#             i+=1
+#         return ans%(10**9+7)
+#         """
+#         :type arr: List[int]
+#         :rtype: int
+#         """
         
