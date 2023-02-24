11ceb1d47ec2c92a612d3374a2d7a8311530a4d9
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-02-24 16:50:05 +0300

    Update 904-fruit-into-baskets.py

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,19 +1,39 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         window = {}
-        j = 0
+        left = 0
         maxLen = 0
-        for i in range(len(fruits)):
-            if fruits[i] not in window:
-                window[fruits[i]] = 1
+        for right,fruit in enumerate(fruits):
+            if fruit in window:
+                window[fruit]+=1
             else:
-                window[fruits[i]] += 1
-            while len(window) > 2:
-                if window[fruits[j]] > 1:
-                    window[fruits[j]] -= 1
-                else:
-                    del window[fruits[j]]
-                j+=1
-            maxLen = max(maxLen,i-j+1)
+                window[fruit] = 1
+            while len(window)>2:
+                if window[fruits[left]]>1:
+                    window[fruits[left]]-=1
+                elif window[fruits[left]] == 1:
+                    del window[fruits[left]]
+                left+=1
+            maxLen = max(maxLen,right-left+1)
         return maxLen
+
+
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+#         window = {}
+#         j = 0
+#         maxLen = 0
+#         for i in range(len(fruits)):
+#             if fruits[i] not in window:
+#                 window[fruits[i]] = 1
+#             else:
+#                 window[fruits[i]] += 1
+#             while len(window) > 2:
+#                 if window[fruits[j]] > 1:
+#                     window[fruits[j]] -= 1
+#                 else:
+#                     del window[fruits[j]]
+#                 j+=1
+#             maxLen = max(maxLen,i-j+1)
+#         return maxLen
             
