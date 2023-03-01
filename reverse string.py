588f8ab6c725ca7af9fc65f61ba233c39f2b5b4f
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-01 11:02:46 +0300

    Update reverse string.py

--- reverse string.py
+++ reverse string.py
@@ -1,7 +1,21 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s)//2):
-            s[i],s[-i-1] = s[-i-1],s[i]
+        def helper(s,i):
+            if i>=(len(s)//2):
+                return 
+            s[i],s[len(s)-i-1] = s[len(s)-i-1],s[i]
+            helper(s,i+1)
+        helper(s,0)
         """
         Do not return anything, modify s in-place instead.
         """
+        
+
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         for i in range(len(s)//2):
+#             s[i],s[-i-1] = s[-i-1],s[i]
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
