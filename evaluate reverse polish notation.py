ebb9327df0271a7d530f613092695ccbc0fbdbfa
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-01 15:04:35 +0300

    Update evaluate reverse polish notation.py

--- evaluate reverse polish notation.py
+++ evaluate reverse polish notation.py
@@ -1,21 +1,28 @@
-import math
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        nums = []
-        for i in tokens:
-            if i.strip("-").isdigit():
-                nums.append(int(i))
+        stack = []
+        for t in tokens:
+            temp = t.lstrip('-')
+            if temp.isdigit():
+                digit = True
             else:
-                num1 = nums.pop()
-                num2 = nums.pop()
-                if i == "/":
-                    temp = num2/num1
-                    if temp > 0:
-                        temp = math.floor(temp)
-                    else:
-                        temp = math.ceil(temp)
-                else:
-                    temp  = eval(str(num2) + i + str(num1))
-                nums.append(temp)
+                digit = False
+            
+            if digit:
+                stack.append(int(t))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if t == "+":
+                    stack.append(num1+num2)
+                elif t == "-":
+                    stack.append(num1-num2)
+                elif t=="*":
+                    stack.append(num1*num2)
+                elif t=="/":
+                    res = num1/num2
+                    res = int(res)
+                    stack.append(int(num1/num2))
+        return stack[0]
+                    
                 
-        return nums[0]
