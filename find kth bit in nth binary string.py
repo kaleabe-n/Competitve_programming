dc9d25b230e0fe93d92863668da50179b59a04a2
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-03 17:47:11 +0300

    Update find kth bit in nth binary string.py

--- find kth bit in nth binary string.py
+++ find kth bit in nth binary string.py
@@ -2,16 +2,19 @@ class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         def helper(n):
             if n==1:
-                return "0"
+                return ["0"]
             else:
                 temp = helper(n-1)
-                temp2 = ""
+                temp2 = []
                 for i in temp:
                     if i=="0":
-                        temp2+="1"
+                        temp2.append("1")
                     else:
-                        temp2+="0"
-                return temp + "1" + temp2[::-1]
+                        temp2.append("0")
+                temp2.reverse()
+                temp.append("1")
+                temp.extend(temp2)
+                return temp 
+            
         ans = helper(n)
         return ans[k-1]
-        
