44e4ee23cd5696e77417cc2e079134b71648ea07
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-06 17:37:46 +0300

    Update pow(x,n).py

--- pow(x,n).py
+++ pow(x,n).py
@@ -1,17 +1,35 @@
-class Solution(object):
-    def myPow(self, x, n):
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         def power(num,exp):
-            if exp<10:
-                return num**exp
+            if exp == 0:
+                return 1
+            if exp<0:
+                return 1/power(num,-exp)
+            if exp==1:
+                return num
             else:
                 if exp%2 == 0:
-                    return power(num,exp/2)**2
+                    temp = power(num,exp/2)
+                    return temp*temp
                 else:
-                    return power(num,exp//2)**2*num
+                    temp = power(num,exp//2)
+                    return temp*temp*num
         return power(x,n)
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
+
+# class Solution(object):
+#     def myPow(self, x, n):
+#         def power(num,exp):
+#             if exp<10:
+#                 return num**exp
+#             else:
+#                 if exp%2 == 0:
+#                     return power(num,exp/2)**2
+#                 else:
+#                     return power(num,exp//2)**2*num
+#         return power(x,n)
+#         """
+#         :type x: float
+#         :type n: int
+#         :rtype: float
+#         """
         
