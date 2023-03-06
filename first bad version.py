41a6090a9ab0f7dbc9061dce7eb724f88efedcbf
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-06 17:44:13 +0300

    Update first bad version.py

--- first bad version.py
+++ first bad version.py
@@ -1,23 +1,40 @@
 # The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return a bool
-# def isBadVersion(version):
+# def isBadVersion(version: int) -> bool:
 
-class Solution(object):
-    def firstBadVersion(self, n):
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
         left = 1
         right = n
-        while right-left>0:
-            if not isBadVersion((left+right)//2):
-                if isBadVersion((left+right)//2+1):
-                    return (left+right)//2+1
-                else:
-                    left = (left+right)//2+1
+        while left<=right:
+            mid = (left+right)//2
+            if isBadVersion(mid):
+                right = mid - 1
             else:
-                right = (left+right)//2
+                left = mid + 1
         return left
-        """
-        :type n: int
-        :rtype: int
-        """
+
+
+
+# # The isBadVersion API is already defined for you.
+# # @param version, an integer
+# # @return a bool
+# # def isBadVersion(version):
+
+# class Solution(object):
+#     def firstBadVersion(self, n):
+#         left = 1
+#         right = n
+#         while right-left>0:
+#             if not isBadVersion((left+right)//2):
+#                 if isBadVersion((left+right)//2+1):
+#                     return (left+right)//2+1
+#                 else:
+#                     left = (left+right)//2+1
+#             else:
+#                 right = (left+right)//2
+#         return left
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
         
