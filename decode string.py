2c532e89bbce3814e7994529781ccd51a6dae56d
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-08 11:03:21 +0300

    Update decode string.py

--- decode string.py
+++ decode string.py
@@ -1,36 +1,66 @@
-class Solution(object):
-    def decodeString(self, s):
-        sta = []
-        string = ""
-        indSta = []
-        num = ""
-        prevIsDigit = False
-        for i in range(len(s)):
-            setNow = False
-            if len(sta) == 0 and s[i].isdigit():
-                num += s[i]
-                prevIsDigit = True
-                setNow = True
-            elif s[i] == "[":
-                sta.append("[")
-                indSta.append(i)
-            elif s[i] == "]":
-                sta.pop()
-                temp = indSta.pop()
-                if len(sta) == 0:
-                    if num is not "":
-                        string += int(num)*str(self.decodeString(s[temp+1:i]))
-                    else:
-                        string+=self.decodeString(s[temp+1:i])
-                    num = ""
-            elif len(sta) == 0:
-                string+=s[i]
-            if not setNow:
-                prevIsDigit = False
-        return string
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def helper(s,mult):
+            ans = []
+            stack = []
+            numStack = []
+            i = 0
+            num = []
+            while i < len(s):
+                letter = s[i]
+                if letter.isdigit():
+                    num.append(letter)
+                elif letter == "[":
+                    stack.append(i+1)
+                    numStack.append(int("".join(num)))
+                    num = []
+                elif letter == "]":
+                    start = stack.pop()
+                    n = numStack.pop()
+                    if not stack:
+                        ans.extend(helper(s[start:i],n))
+                        
+                elif not stack:
+                    ans.append(letter)
+                i+=1
+            ans = ans*mult
+            return ans
+        
+        return "".join(helper(s,1))
+
+# class Solution(object):
+#     def decodeString(self, s):
+#         sta = []
+#         string = ""
+#         indSta = []
+#         num = ""
+#         prevIsDigit = False
+#         for i in range(len(s)):
+#             setNow = False
+#             if len(sta) == 0 and s[i].isdigit():
+#                 num += s[i]
+#                 prevIsDigit = True
+#                 setNow = True
+#             elif s[i] == "[":
+#                 sta.append("[")
+#                 indSta.append(i)
+#             elif s[i] == "]":
+#                 sta.pop()
+#                 temp = indSta.pop()
+#                 if len(sta) == 0:
+#                     if num is not "":
+#                         string += int(num)*str(self.decodeString(s[temp+1:i]))
+#                     else:
+#                         string+=self.decodeString(s[temp+1:i])
+#                     num = ""
+#             elif len(sta) == 0:
+#                 string+=s[i]
+#             if not setNow:
+#                 prevIsDigit = False
+#         return string
                 
-        """
-        :type s: str
-        :rtype: str
-        """
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
         
