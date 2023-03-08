d4d41d83be006dd41d06f37c77c37189ddddc700
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-08 10:38:27 +0300

    Create k-th symbol in grammar.py

---
+++ k-th symbol in grammar.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        if k == 0 or k == 1:
+            return 0
+        if k%2==1:
+            return self.kthGrammar(n-1,(k+1)//2)
+        else:
+            return 1-self.kthGrammar(n-1,k//2)
