53a9c028e4e73970b6b8b9bd37f1b66fd43559ad
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-10 15:51:22 +0300

    Update find the smallest divisor given a threshold.py

--- find the smallest divisor given a threshold.py
+++ find the smallest divisor given a threshold.py
@@ -1,23 +1,49 @@
 import math
 
-class Solution(object):
-    def smallestDivisor(self, nums, threshold):
-        right = max(nums)
-        left = 1
-        while right>left:
-            summ = 0
-            mid = (left+right)//2
-            for i in nums:
-                summ+=math.ceil(float(i)/mid)
-            if summ>threshold:
-                left = (left+right)//2+1
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        l = 1
+        r = max(nums)
+        while l<=r:
+            mid = (l+r)//2
+            currSum = dividendSum(nums,mid)
+            if currSum<=threshold:
+                r=mid-1
             else:
-                right = (left+right)//2
-        return right
+                l=mid+1
+        return r+1
+            
+            
+def dividendSum(arr,num):
+    summ = 0
+    for n in arr:
+        summ+=int(math.ceil(n/num))
+    return summ
+    
+
+
+
+
+# import math
+
+# class Solution(object):
+#     def smallestDivisor(self, nums, threshold):
+#         right = max(nums)
+#         left = 1
+#         while right>left:
+#             summ = 0
+#             mid = (left+right)//2
+#             for i in nums:
+#                 summ+=math.ceil(float(i)/mid)
+#             if summ>threshold:
+#                 left = (left+right)//2+1
+#             else:
+#                 right = (left+right)//2
+#         return right
                 
-        """
-        :type nums: List[int]
-        :type threshold: int
-        :rtype: int
-        """
+#         """
+#         :type nums: List[int]
+#         :type threshold: int
+#         :rtype: int
+#         """
         
