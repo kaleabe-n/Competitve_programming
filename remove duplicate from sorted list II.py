81723d57549794d156860a32bd4a25da5b3f920c
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-21 20:01:22 +0300

    Update remove duplicate from sorted list II.py

--- remove duplicate from sorted list II.py
+++ remove duplicate from sorted list II.py
@@ -5,36 +5,65 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
+        if not head:
             return
-        if head.next  is None:
-            return head
-        if head.next.val == head.val:
-            temp = head
-            while temp is not None and temp.val == head.val:
-                temp = temp.next
-            head.val = None
-            head.next = temp
-            if temp is None:
-                return
-        curr = head
-        prev = None
-        while curr is not None and curr.next is not None:
-            repeated = False
-            if curr.val != curr.next.val:
-                prev = curr
-                temp = curr.next
-                #repeatedNode will be the last of the repeated nodes now it just holds the node before the repeated node
-                repeatedNode = curr
-                while temp.next is not None and temp.val == temp.next.val:
-                    repeated = True
-                    temp = temp.next
-                    repeatedNode = repeatedNode.next
-                if repeated:
-                    repeatedNode.next = None
-                    prev.next = temp.next
-                else:
-                    curr = curr.next
-        return head if head.val is not None else head.next
+        return helper(head)[0]
+        
+        
+def helper(node):
+    if not node.next:
+        return node,None
+    n,toRemove = helper(node.next)
+    if n and n.val == node.val:
+        return n.next,n.val
+    elif node.val == toRemove:
+        return n,toRemove
+    node.next = n
+    return node,None
+
+
+
+
+
+
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if head is None:
+#             return
+#         if head.next  is None:
+#             return head
+#         if head.next.val == head.val:
+#             temp = head
+#             while temp is not None and temp.val == head.val:
+#                 temp = temp.next
+#             head.val = None
+#             head.next = temp
+#             if temp is None:
+#                 return
+#         curr = head
+#         prev = None
+#         while curr is not None and curr.next is not None:
+#             repeated = False
+#             if curr.val != curr.next.val:
+#                 prev = curr
+#                 temp = curr.next
+#                 #repeatedNode will be the last of the repeated nodes now it just holds the node before the repeated node
+#                 repeatedNode = curr
+#                 while temp.next is not None and temp.val == temp.next.val:
+#                     repeated = True
+#                     temp = temp.next
+#                     repeatedNode = repeatedNode.next
+#                 if repeated:
+#                     repeatedNode.next = None
+#                     prev.next = temp.next
+#                 else:
+#                     curr = curr.next
+#         return head if head.val is not None else head.next
                     
                 
