78341e933a9ecfec49fc010c997a6cf50cea05f9
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-03-24 17:55:49 +0300

    Update 486-predict-the-winner.py

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,14 +1,30 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         dp = {}
-        def chooseBest(left,right,dp):
-            if left == right:
-                return nums[left]
-            if (left,right) in dp:
-                return dp[(left,right)]
-            bestLeft = nums[left] - chooseBest(left+1,right,dp)
-            bestRight = nums[right] - chooseBest(left,right-1,dp)
-            dp[(left,right)] = max(bestLeft,bestRight)
-            return max(bestLeft,bestRight)
-        return chooseBest(0,len(nums)-1,dp)>=0
+        return helper(0,len(nums)-1,nums,dp)>=0
+        
+def helper(l,r,nums,dp):
+    if r == l:
+        return nums[l]
+    if (l,r) in dp:
+        return dp[(l,r)]
+    left = nums[l] - helper(l+1,r,nums,dp)
+    right = nums[r] - helper(l,r-1,nums,dp)
+    dp[(l,r)] = max(left,right)
+    return max(left,right)
+
+
+# class Solution:
+#     def PredictTheWinner(self, nums: List[int]) -> bool:
+#         dp = {}
+#         def chooseBest(left,right,dp):
+#             if left == right:
+#                 return nums[left]
+#             if (left,right) in dp:
+#                 return dp[(left,right)]
+#             bestLeft = nums[left] - chooseBest(left+1,right,dp)
+#             bestRight = nums[right] - chooseBest(left,right-1,dp)
+#             dp[(left,right)] = max(bestLeft,bestRight)
+#             return max(bestLeft,bestRight)
+#         return chooseBest(0,len(nums)-1,dp)>=0
         
