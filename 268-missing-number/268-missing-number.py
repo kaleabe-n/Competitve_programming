5b37ab006d8b9dc5cd5841dc16ffd09119e97c64
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-04-04 21:35:03 +0300

    Update 268-missing-number.py

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,15 +1,34 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        summ = sum(nums)
-        expSum = 0
+        # return [x for x in range(len(nums)+1) if x not in set(nums)][0]
         i = 0
-        while i<=len(nums):
-            expSum+=i
-            i+=1
-        i = 0
-        while i<=len(nums):
-            if summ+i == expSum:
+        while i <len(nums):
+            if nums[i]==len(nums):
+                i+=1
+                continue
+            elif nums[i]!=i:
+                nums[nums[i]],nums[i] = nums[i],nums[nums[i]]
+            else:
+                i+=1
+        for i,num in enumerate(nums):
+            if i!=num:
                 return i
-            i+=1
+        return len(nums)
+        
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         summ = sum(nums)
+#         expSum = 0
+#         i = 0
+#         while i<=len(nums):
+#             expSum+=i
+#             i+=1
+#         i = 0
+#         while i<=len(nums):
+#             if summ+i == expSum:
+#                 return i
+#             i+=1
             
         
