e23b75e826ce44557f12eaa788368b92f047cd21
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-04-04 18:21:56 +0300

    Update kth largest element in an array.py

--- kth largest element in an array.py
+++ kth largest element in an array.py
@@ -1,17 +1,49 @@
-import heapq
+import random
 
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        heap = []
-        for i in range(k):
-            heapq.heappush(heap,nums[i])
-        for i in range(k,len(nums)):
-            if nums[i]>heap[0]:
-                heapq.heappushpop(heap,nums[i])
-        return heap[0]
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return helper(nums,k)
+        
+def helper(arr,k):
+    if len(arr) == 1:
+        return arr[0]
+    ind = random.randint(0,len(arr)-1)
+    num = arr[ind]
+    left = []
+    right = []
+    for i in range(0,len(arr)):
+        if i==ind:
+            continue
+        if arr[i] >= num:
+            right.append(arr[i])
+        else:
+            left.append(arr[i])
+            
+    if len(right)>=k:
+        return helper(right,k)
+    elif len(right)+1 == k:
+        return num
+    else:
+        return helper(left,k-len(right)-1
+
+                      
+                      
+                      
+
+# import heapq
+
+# class Solution(object):
+#     def findKthLargest(self, nums, k):
+#         heap = []
+#         for i in range(k):
+#             heapq.heappush(heap,nums[i])
+#         for i in range(k,len(nums)):
+#             if nums[i]>heap[0]:
+#                 heapq.heappushpop(heap,nums[i])
+#         return heap[0]
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
         
