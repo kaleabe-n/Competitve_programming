9436dd16774b493cb8efaf2148709231fd1187ec
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-04-10 18:18:27 +0300

    Update maximum depth of n-ary tree.py

--- maximum depth of n-ary tree.py
+++ maximum depth of n-ary tree.py
@@ -1,25 +1,48 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
 """
 
-class Solution(object):
-    def maxDepth(self, root):
-        def counter(node,count,maxx):
-            if node is None:
-                return count
-            if len(node.children) == 0:
-                return count+1
-            for child in node.children:
-                ans = counter(child,count+1,maxx)
-                maxx = max(maxx,ans)
-            return maxx
-        return counter(root,0,0)
-        """
-        :type root: Node
-        :rtype: int
-        """
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        return helper(root)
+        
+        
+def helper(node):
+    if not node:
+        return 0
+    maxx = 0
+    for child in node.children:
+        maxx = max(maxx,helper(child))
+    return maxx+1
+
+
+
+# """
+# # Definition for a Node.
+# class Node(object):
+#     def __init__(self, val=None, children=None):
+#         self.val = val
+#         self.children = children
+# """
+
+# class Solution(object):
+#     def maxDepth(self, root):
+#         def counter(node,count,maxx):
+#             if node is None:
+#                 return count
+#             if len(node.children) == 0:
+#                 return count+1
+#             for child in node.children:
+#                 ans = counter(child,count+1,maxx)
+#                 maxx = max(maxx,ans)
+#             return maxx
+#         return counter(root,0,0)
+#         """
+#         :type root: Node
+#         :rtype: int
+#         """
         
