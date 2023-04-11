1c0caaa636a4306c815ad1cca67fe5486d8e5ca8
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-04-11 18:12:15 +0300

    Update number of provinces.py

--- number of provinces.py
+++ number of provinces.py
@@ -1,25 +1,46 @@
-from collections import defaultdict
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        nonVisited = set(range(len(isConnected)))
+        provinces = 0
+        while nonVisited:
+            root = nonVisited.pop()
+            stack = [root]
+            provinces+=1
+            while stack:
+                curr = stack.pop()
+                for i in range(len(isConnected)):
+                    if i in nonVisited and i!=curr and isConnected[curr][i]:
+                        stack.append(i)
+                        nonVisited.remove(i)
+                        
+        return provinces
+                
 
-class Solution(object):
-    def findCircleNum(self, isConnected):
-        visited = defaultdict(lambda:False)
-        count = 0
-        for j in range(len(isConnected)):
-            if not visited[j]:
-                count+=1
-                dfs(j,visited,isConnected)
-        return count
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
-def dfs(num,visited,isConnected):
-    stack = [num]
-    visitedDfs = defaultdict(lambda:False)
-    while stack:
-        temp = stack.pop()
-        visitedDfs[temp] = True
-        visited[temp] = True
-        for i in range(len(isConnected)):
-            if not visitedDfs[i] and isConnected[temp][i]==1:
-                stack.append(i)
+
+
+
+# from collections import defaultdict
+
+# class Solution(object):
+#     def findCircleNum(self, isConnected):
+#         visited = defaultdict(lambda:False)
+#         count = 0
+#         for j in range(len(isConnected)):
+#             if not visited[j]:
+#                 count+=1
+#                 dfs(j,visited,isConnected)
+#         return count
+#         """
+#         :type isConnected: List[List[int]]
+#         :rtype: int
+#         """
+# def dfs(num,visited,isConnected):
+#     stack = [num]
+#     visitedDfs = defaultdict(lambda:False)
+#     while stack:
+#         temp = stack.pop()
+#         visitedDfs[temp] = True
+#         visited[temp] = True
+#         for i in range(len(isConnected)):
+#             if not visitedDfs[i] and isConnected[temp][i]==1:
+#                 stack.append(i)
