0fac1e2bcfda4b0d9b7295bee4aa2e52d78e0160
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-04-28 17:55:18 +0300

    Update 841-keys-and-rooms.py

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -1,12 +1,26 @@
+# class Solution:
+#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+#         visited = set([0])
+#         stack = [0]
+#         while stack:
+#             temp = stack.pop()
+#             for i in rooms[temp]:
+#                 if i not in visited:
+#                     stack.append(i)
+#                     visited.add(i)
+#         return len(visited) == len(rooms)
+        
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        que = collections.deque([0])
         visited = set([0])
-        stack = [0]
-        while stack:
-            temp = stack.pop()
-            for i in rooms[temp]:
-                if i not in visited:
-                    stack.append(i)
-                    visited.add(i)
-        return len(visited) == len(rooms)
-        
+        while que:
+            curr = que.popleft()
+            for room in rooms[curr]:
+                if room not in visited:
+                    visited.add(room)
+                    que.append(room)
+                    
+        if len(visited) == len(rooms):
+            return True
+        return False
