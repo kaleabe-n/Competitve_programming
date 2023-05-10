b89e0b52b038d4c17b8262fd74de7e5127b55739
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-05-10 19:12:40 +0300

    Update 1962-remove-stones-to-minimize-the-total.py

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -1,12 +1,24 @@
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        for i in range(len(piles)):
-            piles[i] = -piles[i]
+        piles = [-x for x in piles]
         heapq.heapify(piles)
-        while k>0:
-            maxx = -heapq.heappop(piles)
-            maxx = ceil(maxx/2)
-            heapq.heappush(piles,-maxx)
-            k-=1
+        for _ in range(k):
+            curr = heapq.heappop(piles)
+            curr = -curr
+            curr -= curr//2
+            heapq.heappush(piles,-curr)
+        
         return -sum(piles)
+
+# class Solution:
+#     def minStoneSum(self, piles: List[int], k: int) -> int:
+#         for i in range(len(piles)):
+#             piles[i] = -piles[i]
+#         heapq.heapify(piles)
+#         while k>0:
+#             maxx = -heapq.heappop(piles)
+#             maxx = ceil(maxx/2)
+#             heapq.heappush(piles,-maxx)
+#             k-=1
+#         return -sum(piles)
         
