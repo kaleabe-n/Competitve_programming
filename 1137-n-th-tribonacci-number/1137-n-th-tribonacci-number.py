5e3aa8a4e9c853a901d8b476773b50a8366bf014
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-05-30 19:51:26 +0300

    Update 1137-n-th-tribonacci-number.py

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,11 +1,24 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        tribs = [0,1,1]
+        nums = [0,1,1]
         if n<3:
-            return tribs[n]
+            return nums[n]
         for i in range(3,n+1):
-            if i == n:
-                return sum(tribs)
-            curr = sum(tribs)
-            tribs.pop(0)
-            tribs.append(curr)
+            curr = sum(nums)
+            nums.pop(0)
+            nums.append(curr)
+        return nums[-1]
+
+
+
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         tribs = [0,1,1]
+#         if n<3:
+#             return tribs[n]
+#         for i in range(3,n+1):
+#             if i == n:
+#                 return sum(tribs)
+#             curr = sum(tribs)
+#             tribs.pop(0)
+#             tribs.append(curr)
