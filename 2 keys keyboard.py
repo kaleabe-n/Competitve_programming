03c9d5a2b1980b6558cf28da6caf8a816b731507
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-09-25 16:08:23 +0300

    Update 2 keys keyboard.py

--- 2 keys keyboard.py
+++ 2 keys keyboard.py
@@ -1,13 +1,40 @@
+# class Solution:
+#     def minSteps(self, n: int) -> int:
+#         primes = []
+#         for i in range(2,n//2):
+#             while n%i == 0:
+#                 primes.append(i)
+#                 n//=i
+#         if n>1:
+#             primes.append(n)
+            
+#         return sum(primes)
+
+
+
+#with dp below with math above
+
+
 class Solution:
     def minSteps(self, n: int) -> int:
-        primes = []
-        for i in range(2,n//2):
-            while n%i == 0:
-                primes.append(i)
-                n//=i
-        if n>1:
-            primes.append(n)
-            
-        return sum(primes)
-            
+        clip = 0
+        dp = [float('inf')]*n
+        dp[0] = 0
+        screen = 1
+        for i in range(n):
+            if dp[i] == float('inf'):
+                continue
+            clip = i+1
+            screen = i
+            count = 1
+            j = 1
+            while True:
+                screen += clip
+                j+=1
+                if screen>=n:
+                    break
+                dp[screen] = min(dp[i] + j,dp[screen])
+                
+        return dp[-1]
+        
             
