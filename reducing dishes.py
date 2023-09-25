d01980ed4ac8b105b47816a7cee81842ebd30ad1
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-09-25 17:47:05 +0300

    Update reducing dishes.py

--- reducing dishes.py
+++ reducing dishes.py
@@ -1,21 +1,38 @@
+# class Solution:
+#     def maxSatisfaction(self, satisfaction: List[int]) -> int:
+#         satisfaction.sort()
+#         ans = 0
+#         prefix = 0
+#         i = len(satisfaction)-1
+#         while i>=0 and satisfaction[i]>=0:
+#             prefix+=satisfaction[i]
+#             ans+=prefix
+#             i-=1
+            
+#         while i>=0:
+#             prefix += satisfaction[i]
+#             if prefix>=0:
+#                 ans+=prefix
+#             else:
+#                 break
+#             i-=1
+#         return ans
+        
+        
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()
-        ans = 0
-        prefix = 0
-        i = len(satisfaction)-1
-        while i>=0 and satisfaction[i]>=0:
-            prefix+=satisfaction[i]
-            ans+=prefix
-            i-=1
+        prefix = [0]
+        for num in satisfaction:
+            prefix.append(prefix[-1]+num)
+        prefix.pop(0)
+        allDishes = 0
+        for i in range(len(satisfaction)):
+            allDishes+=(i+1)*satisfaction[i]
+        dp = [0]*len(satisfaction)
+        dp[0] = allDishes
+        for i in range(1,len(satisfaction)):
+            dp[i] = dp[i-1]-(prefix[-1]-prefix[i-1])-satisfaction[i-1]
+        ans = max(dp)
+        return ans if ans>=0 else 0
             
-        while i>=0:
-            prefix += satisfaction[i]
-            if prefix>=0:
-                ans+=prefix
-            else:
-                break
-            i-=1
-        return ans
-        
-        
