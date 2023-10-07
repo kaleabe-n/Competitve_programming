d6539ee14e3a968e4fdfe4f70e58dca937c75a1c
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-10-07 15:04:51 +0300

    Update path sum III.py

--- path sum III.py
+++ path sum III.py
@@ -1,3 +1,48 @@
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+#         if not root:
+#             return 0
+#         stack = [root]
+#         total =  0
+#         while stack:
+#             curr = stack.pop()
+#             total+=dfs(curr,targetSum)
+#             if curr.left:
+#                 stack.append(curr.left)
+#             if curr.right:
+#                 stack.append(curr.right)
+            
+#         return total
+            
+        
+        
+# def dfs(node,target):
+#     left = target-node.val
+#     if not node.right and not node.left:
+#         if left == 0:
+#             return 1
+#         return 0
+#     if left == 0:
+#         curr = 1
+#     else:
+#         curr = 0
+#     if node.right:
+#         curr += dfs(node.right,left)
+#     if node.left:
+#         curr += dfs(node.left,left)
+        
+#     return curr
+
+
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,33 +53,21 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if not root:
             return 0
-        stack = [root]
-        total =  0
-        while stack:
-            curr = stack.pop()
-            total+=dfs(curr,targetSum)
-            if curr.left:
-                stack.append(curr.left)
-            if curr.right:
-                stack.append(curr.right)
-            
-        return total
-            
+        return dfs(root,[],targetSum)
         
-        
-def dfs(node,target):
-    left = target-node.val
-    if not node.right and not node.left:
-        if left == 0:
-            return 1
-        return 0
-    if left == 0:
-        curr = 1
-    else:
-        curr = 0
-    if node.right:
-        curr += dfs(node.right,left)
+def dfs(node,prevSums,target):
+    count = 0
+    for i,s in enumerate(prevSums):
+        prevSums[i] += node.val
+        if prevSums[i] == target:
+            count += 1
+    if node.val == target:
+        count += 1
+    prevSums.append(node.val)
     if node.left:
-        curr += dfs(node.left,left)
-        
-    return curr
+        count += dfs(node.left,prevSums.copy(),target)
+    if node.right:
+        count += dfs(node.right,prevSums,target)
+    return count
+    
+    
