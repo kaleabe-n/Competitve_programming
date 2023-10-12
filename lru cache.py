0d1c86cd889d3832eb2a56bbf0a1ea30f61d1af5
Kaleabe Negussie <91533360+kaleabe-n@users.noreply.github.com>
2023-10-12 14:56:13 +0300

    Update lru cache.py

--- lru cache.py
+++ lru cache.py
@@ -1,3 +1,67 @@
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.heap = []
+        self.count = collections.defaultdict(int)
+        self.mapping = {}
+        self.prevs = 0
+        """
+        :type capacity: int
+        """
+        
+
+    def get(self, key):
+        if key in self.mapping:
+            heapq.heappush(self.heap,[self.prevs,key])
+            self.prevs+=1
+            self.count[key]+=1
+            return self.mapping[key]
+        return -1
+        """
+        :type key: int
+        :rtype: int
+        """
+        
+
+    def put(self, key, value):
+        if key in self.mapping:
+            heapq.heappush(self.heap,[self.prevs,key])
+            self.prevs+=1
+            self.count[key]+=1
+            self.mapping[key] = value
+            return 
+        elif len(self.mapping.keys()) == self.capacity:
+            _,toremove = heapq.heappop(self.heap)
+            self.count[toremove]-=1
+            while self.count[toremove]>0:
+                _,toremove = heapq.heappop(self.heap)
+                self.count[toremove]-=1
+            self.mapping.pop(toremove)
+        self.mapping[key] = value
+        self.count[key] += 1
+        heapq.heappush(self.heap,[self.prevs,key])
+        self.prevs+=1
+        
+            
+            
+                
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+
+
 # from collections import OrderedDict
 
 # class LRUCache(object):
